b6506eb3fd955611e63ba28ed50b5863eeb70e54
Beatriz Moreli <79378960+Beamoreli@users.noreply.github.com>
2022-03-11 16:29:59 -0300

    Add files via upload

---
+++ ProjetoGenMusicvs1.0/src/ExecucaoMusicas/Login.java
@@ -0,0 +1,34 @@
+package ExecucaoMusicas;
+
+public class Login {
+
+	private String nome;
+	private int senha;
+	private String login;
+	private int dataNascimento;
+	   
+	   public String getNome() {
+		   return nome;
+	   }
+	   
+	   public void setNome(String name) {
+		   nome = name;
+	   }
+	   
+	   public String getLogin() {
+		   return login;
+	   }
+	   
+	   public void setLogin(String login) {
+		   login = login;
+	   }
+	   
+	   public int getDataNascimento() {
+		   return dataNascimento;
+	   }
+	   
+	   public void setDataNascimento(int dataNascimento) {
+		   dataNascimento = dataNascimento;
+	   }
+	     
+}
