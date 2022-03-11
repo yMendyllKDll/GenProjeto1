1fe0af9ff76615b5266680b8ee746028424484b6
Beatriz Moreli <79378960+Beamoreli@users.noreply.github.com>
2022-03-10 21:14:43 -0300

    Add files via upload

---
+++ JavaProjeto/ProjetoMusica/src/Login.java
@@ -0,0 +1,34 @@
+package org.generation.brazil;
+
+public class Login {
+ 
+	public String nome;
+	public int senha;
+	public String login;
+	public int dataNascimento;
+	   
+	   public String getNome() {
+		   return nome;
+	   }
+	   
+	   public void setNome(String name) {
+		   nome = name;
+	   }
+	   
+	   public String getLogin() {
+		   return login;
+	   }
+	   
+	   public void setLogin(String login) {
+		   login = login;
+	   }
+	   
+	   public int getDataNascimento() {
+		   return dataNascimento;
+	   }
+	   
+	   public void setDataNascimento(int dataNascimento) {
+		   dataNascimento = dataNascimento;
+	   }
+	     
+}
---
+++ JavaProjeto/ProjetoMusica/src/LoginObjeto.java
@@ -0,0 +1,45 @@
+package org.generation.brazil;
+
+import java.util.Scanner;
+
+public class LoginObjeto extends Login{
+
+
+	public static void main(String[] args) {
+		
+
+		Scanner ler = new Scanner(System.in);
+		
+		
+        System.out.println("Você já tem um cadastro?");
+		System.out.println("Digite |1| para sim e |2| para não: ");
+		int cadastro  = ler.nextInt();
+		System.out.println();
+
+		if (cadastro == 1) {
+			System.out.println("Digite o Login: ");
+			String login = ler.next();
+			System.out.println("Digite sua senha: ");
+			int senha = ler.nextInt();
+			System.out.println("Você está dentro da biblioteca.");
+		}
+		
+		else {
+			System.out.println("Cadastro de usuario");
+			System.out.println("Digite os dados:\n"+"Nome: ");
+	        String nome = ler.next();
+	        System.out.println("Login (e-mail): ");
+	        String login = ler.next();
+	        System.out.println("Data de nascimento ");
+	        int dataNascimento= ler.nextInt();
+	        System.out.println("Senha de acesso");
+	        int senha = ler.nextInt();
+	        
+	        System.out.println("Cadastro finalizado: ");
+	        System.out.println("Seja bem vindo" + nome);
+	        
+		}
+        
+	}
+	
+}
